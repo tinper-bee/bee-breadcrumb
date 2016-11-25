b0bc46bcd491a4be1e2431614357de75f7344da6
ahua52 <1468492018@qq.com>
2016-11-25 16:14:22 +0800

    Breadcrumb

---
+++ demo/index.js
@@ -0,0 +1,3 @@
+import Demo from './BreadcrumbDemo';
+import ReactDOM from 'react-dom';
+ReactDOM.render(<Demo/>, document.getElementById('tinperBeeDemo'));
---
+++ src/BreadcrumbItem.js
@@ -0,0 +1,44 @@
+import classNames from 'classnames';
+import React ,{ PropTypes }from 'react';
+
+
+const propTypes = {
+  /**
+   * 如果设置成true渲染`span` 而不是 `a`
+   */
+  active: PropTypes.bool,
+  /**
+   * `href` a标签href
+   */
+  href: PropTypes.string,
+  /**
+   * `title`  a标签title
+   */
+  title: PropTypes.node,
+};
+
+const defaultProps = {
+  active: false,
+};
+
+class BreadcrumbItem extends React.Component {
+  render() {
+    const { active, href, title, target, className, ...props } = this.props;
+
+    const linkProps = { href, title };
+
+    return (
+      <li className={classNames(className, { active })}>
+        {active ?
+          <span {...props} /> :
+          <a {...props} {...linkProps} />
+        }
+      </li>
+    );
+  }
+}
+
+BreadcrumbItem.propTypes = propTypes;
+BreadcrumbItem.defaultProps = defaultProps;
+
+export default BreadcrumbItem;
